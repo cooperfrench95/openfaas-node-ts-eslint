bf3dd9ce625cf18d9d8e178b470d77e25518af0b
Cooper French <cooperfrench95@gmail.com>
2020-05-07 16:43:35 +0800

    initial commit

---
+++ function/handler.js
@@ -0,0 +1,9 @@
+'use strict'
+
+module.exports = async (event, context) => {
+    const result = {
+        status: 'Received input: ' + JSON.stringify(event.body),
+    }
+
+    return context.status(200).succeed(result)
+}
